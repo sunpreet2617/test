3e34f9c832c7499053f844f496aff11eddaaa647
unknown <surajk@lambdatest.com>
2018-09-12 21:25:35 +0530

    Fixes

--- EV2.py
+++ EV2.py
@@ -6,25 +6,24 @@ import smtplib
 
 from openpyxl import load_workbook
 
-wb =load_workbook('test.xlsx', data_only= True)
+wb = load_workbook('test.xlsx', data_only= True)
 sh = wb["Sheet1"]
 
 
 for row in sh['A{}:A{}'.format(sh.min_row+1,sh.max_row)]:
 	for cell in row:
-                
 		wb.save('test.xlsx')
 		addressToVerify = cell.value
 		match = re.match('^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$', addressToVerify)
 		if match == None:
 			print('Bad Syntax for '+addressToVerify)
-			#raise ValueError('Bad Syntax')
 
 		resolver = dns.resolver.Resolver()
-        records = dns.resolver.query('lambdadirect.com','MX')
+		records = dns.resolver.query('lambdatest.com', 'MX')
+
 		mxRecord = records[0].exchange
 		mxRecord = str(mxRecord)
-     	
+
 # Get local server hostname
 		host = socket.gethostname()
 
@@ -45,15 +44,12 @@ for row in sh['A{}:A{}'.format(sh.min_row+1,sh.max_row)]:
 		if code == 550:
 			sh.cell(row=cell.row,column=2).value="Fail"
 
-    			
 		if code == 250:
 			sh.cell(row=cell.row,column=2).value="Success"
 				
 		else:
 			sh.cell(row=cell.row,column=2).value="Fail"
-                        
 
-				
+
 wb.save('test.xlsx')
-
 print("Done")
